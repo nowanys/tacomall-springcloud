28d158edb8f91c38564c82272fa95a4c404bdabb
码上talk|RC <3189482282@qq.com>
2020-11-19 15:45:35 +0800

    1、添加注册中心认证

--- register/src/main/java/store/tacomall/register/RegisterApplication.java
+++ register/src/main/java/store/tacomall/register/RegisterApplication.java
@@ -1,7 +1,7 @@
 /***
  * @Author: 码上talk|RC
  * @Date: 2020-06-09 23:20:09
- * @LastEditTime: 2020-11-12 17:44:56
+ * @LastEditTime: 2020-11-19 14:46:40
  * @LastEditors: 码上talk|RC
  * @Description: 
  * @FilePath: /tacomall-springcloud/register/src/main/java/store/tacomall/register/RegisterApplication.java
@@ -14,11 +14,23 @@ package store.tacomall.register;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class RegisterApplication {
     public static void main(String[] args) {
         SpringApplication.run(RegisterApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            super.configure(http);
+        }
     }
 }
