ddbb8b7509d1b46513ec96f600b3661f21db6c1d
码上talk|RC <3189482282@qq.com>
2020-12-02 16:02:15 +0800

    1、新增entity、mapper文件自动生成

---
+++ _db/index.py
@@ -0,0 +1,16 @@
+'''
+Author: 码上talk|RC
+Date: 2020-12-02 11:31:22
+LastEditTime: 2020-12-02 11:47:16
+LastEditors: 码上talk|RC
+Description: 
+FilePath: /tacomall-springcloud/_db/index.py
+微信:  13680065830
+邮箱:  3189482282@qq.com
+oops: Just do what I think it is right
+'''
+from gen_entity import GenEntity
+
+if __name__ == '__main__':
+    gen_entity = GenEntity()
+    gen_entity.run()
